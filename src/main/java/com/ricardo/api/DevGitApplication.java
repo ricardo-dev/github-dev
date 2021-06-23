63307c04477d22384bff87ba4c6a224177162651
ricardo-dev <ricardopuc.computacao@gmail.com>
2021-06-23 09:23:24 -0300

    [ADD] inserindo base inicial do projeto

---
+++ src/main/java/com/ricardo/api/DevGitApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/api/DevGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
